334c927530c3f648eb27a37dd6ffb276738bd733
MinchanKwon <ksn5014@snu.ac.kr>
2023-07-06 13:55:14 +0900

    APR1400_YJCHO commit

---
+++ Cpp/abcpp/src/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i = 4133;
+    int a;
+    printf("Write the number\n");
+    scanf("%d",&a);
+    printf("a is %d\n",a);
+
+
+
+    
+    system("pause");
+    return 0;
+}
